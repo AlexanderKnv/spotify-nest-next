febf79475b55a41190a838a29260cabc6a814099
AlexCreatin <konashkov23@gmail.com>
2023-04-19 17:08:52 +0200

    Initialization and project structure

---
+++ server/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT || 3000;
+
+    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();

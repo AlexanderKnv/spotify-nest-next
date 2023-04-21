e0436c90444518d14ac5f09584db8d7f0a1096a5
AlexCreatin <konashkov23@gmail.com>
2023-04-21 21:09:39 +0200

    database schemas inject a model

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Track } from "./schemas/track.schema";
+import { Comment } from "./schemas/comment.schema";
+import { Model } from "mongoose";
+
+
 
 @Injectable()
 export class TrackService {
+
+    constructor(@InjectModel(Track.name) private trackModel: Model<Track>,
+                @InjectModel(Comment.name) private commentModel: Model<Comment>) {}
 
     async create() {
 

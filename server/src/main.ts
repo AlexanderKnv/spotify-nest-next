134b9c79b44b4a6a85915967820ef5e408496d07
AlexCreatin <konashkov23@gmail.com>
2023-04-25 21:06:31 +0200

    frontend initialization

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ const start = async () => {
   try {
     const app = await NestFactory.create(AppModule);
     const PORT = process.env.PORT || 3000;
-
+    app.enableCors()
     await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
   } catch (e) {
     console.log(e);
